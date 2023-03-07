88048a934b671d9af3afd61950b005ef094f481c
MrMarkian <43710879+MrMarkian@users.noreply.github.com>
2023-03-07 19:11:07 +0000

    Started Multiplayer Support

---
+++ ChessLikeGame/Assets/Scripts/Multiplayer/ChessPieceTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Multiplayer
+{
+    [Serializable]
+    public enum ChessPieceTypes
+    {
+        NONE,
+        PAWN,
+        BISHOP,
+        KNIGHT,
+        ROOK,
+        QUEEN,
+        KING 
+    }
+}
---
+++ ChessLikeGame/Assets/Scripts/Multiplayer/GridColor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Multiplayer
+{
+    [Serializable]
+    public enum GridColor
+    {
+        BLACK,
+        WHITE
+    }
+}
